1ad486d7527286fbbad17ce165618ab861ddc9b1
niedziak <71374116+niedziak@users.noreply.github.com>
2020-10-11 17:06:57 +0200

    Update browser_rpg_scripts.js

--- browser_rpg_scripts.js
+++ browser_rpg_scripts.js
@@ -1,11 +1,11 @@
-var plyr_hp = 100;
-var inv = ["HARPOON","KNIFE","LANTERN"]; // inventory
-var invidx = 0;
+var plyr_hp = 100; // player's hp
+var inv = ["Item 1","Item 2","Item 3"]; // inventory
+var invindx = 0; // inventory index
 
 document.getElementById("invWindow").innerHTML = inv[0]; // presenting item at start
 
 // change in player hp
-function hpchange (mindmg,maxdmg,bnsdmg) {
+function hpChange(mindmg,maxdmg,bnsdmg) {
     plyr_hp += (Math.floor(Math.random() * (maxdmg - mindmg + 1) + mindmg)) + bnsdmg;
 
     // hp bounds
@@ -22,11 +22,40 @@ function hpchange (mindmg,maxdmg,bnsdmg) {
 function invChange(chng) {
     invindx += chng;
 
-    if (invidx < 0) {
-        invidx = inv.length - 1;
+    if (invindx <= -1) {
+        invindx = inv.length - 1;
     } else if (invindx >= inv.length) {
         invindx = 0;
     }
 
-    document.getElementById("invWindow").innerHTML = inv[invidx];
+    document.getElementById("invWindow").innerHTML = inv[invindx];
+}
+
+// random events
+function randomEvent() {
+    switch (Math.ceil(Math.random()*10)) {
+        case 1:
+            document.getElementById("rndevtWindow").innerHTML = "Event 1";
+            break;
+        case 2:
+        case 3:
+            document.getElementById("rndevtWindow").innerHTML = "Event 2";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            document.getElementById("rndevtWindow").innerHTML = "Event 3";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            document.getElementById("rndevtWindow").innerHTML = "Event 4";
+            break;
+        case 10:
+            document.getElementById("rndevtWindow").innerHTML = "Event 5";
+            break;  
+        default:
+            document.getElementById("rndevtWindow").innerHTML = "default";
+            break;
+    }
 }
